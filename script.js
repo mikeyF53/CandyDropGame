2ff1ac850d0103a39333e2c2424e5748018b4192
Michael Fang <mikefang530@gmail.com>
2019-02-04 15:29:03 -0500

    candy dropper and walls created

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log('js loaded');
 
+const board = document.querySelector('.gameBoard');
 const startButton = document.querySelector('button');
 const body = document.querySelector('body');
 //so i can remove them after clicking start
@@ -13,6 +14,25 @@ const removeInstrBox = () => {
   }
 startButton.addEventListener('click', (removeInstrBox));
 //////////////Start of the game//////////////
+const startDropper = () => {
+  const dropper = document.createElement('div');
+    dropper.classList.add('candyDropper');
+    board.appendChild(dropper);
+};
+////// Create the walls/////////////
+const mkWalls = () => {
 
-const dropper = document.createElement('div dropper');
-console.log(dropper);
+  const walls = document.createElement('div');
+  const wallLeft = document.createElement('div');
+  const wallRight = document.createElement('div');
+    walls.classList.add('walls');
+    wallLeft.classList.add('wallLeft');
+    wallRight.classList.add('wallRight');
+    console.log(walls);
+    board.appendChild(walls);
+    walls.appendChild(wallLeft);
+    walls.appendChild(wallRight);
+};
+
+startButton.addEventListener('click', (startDropper));
+startButton.addEventListener('click', (mkWalls));
