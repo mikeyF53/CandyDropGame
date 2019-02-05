afb91f484cac1ff24ee6c3c04828d2350b3199ad
Michael Fang <mikefang530@gmail.com>
2019-02-05 10:58:16 -0500

    trying to figure out how to fire candy piece

--- script.js
+++ script.js
@@ -5,25 +5,53 @@ const startButton = document.querySelector('button');
 const body = document.querySelector('body');
 //so i can remove them after clicking start
 // const instrBox = document.querySelector('.scoreInstr');
-
 const section = document.querySelector('section');
 const remBox = document.querySelector('.toRemove')
+// const dropperObject = document.querySelector('.candyDropper');
 
 const removeInstrBox = () => {
   remBox.remove();
   }
 startButton.addEventListener('click', (removeInstrBox));
 
-//////////////Carete the candy dropper//////////////
+//////////////Create the candy dropper//////////////
 const startDropper = () => {
+
   const dropperBox = document.createElement('div');
     dropperBox.classList.add('dropperBox')
-  const dropper = document.createElement('div');
+    dropper = document.createElement('div');
     dropper.classList.add('candyDropper');
     board.appendChild(dropperBox);
     dropperBox.appendChild(dropper);
 
 };
+
+///Create the dropping candy piece
+const candyPiece = () => {
+  const theCandy = document.createElement('div');
+  theCandy.classList.add('theCandy');
+  board.appendChild(theCandy);
+  return theCandy;
+};
+///Set position for candyPiece
+
+const candyPosition = () => {
+  const candyPos = `${dropper.offsetLeft + dropper.offsetWidth / 2}px`;
+  return candyPos;
+  console.log(candyPos);
+}
+
+const candyFire =(ev) => {
+
+  if (ev.keyCode === 32) {
+    console.log("space bar pressed");
+    let theCandy = candyPiece();
+    theCandy.style.left = candyPostion();
+};
+
+};
+  body.addEventListener('keydown', candyFire);
+
 ////// Create the walls/////////////
 const mkWalls = () => {
 
@@ -38,18 +66,11 @@ const mkWalls = () => {
      walls.appendChild(wallRight);
 };
 
-//Create the dropping candy piece///
-const candyPiece = () => {
-  const theCandy = document.createElement('div');
-  theCandy.classList.add('theCandy');
-  board.appendChild(theCandy);
-};
-
 //start the game //
 const beginGame = () => {
   startButton.addEventListener('click', startDropper);
   startButton.addEventListener('click', mkWalls);
-  startButton.addEventListener('click', candyPiece);
+//  startButton.addEventListener('click', candyPiece);
 };
 
 beginGame();
