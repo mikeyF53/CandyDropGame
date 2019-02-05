5b919e829d1692988cc1ef6037b2bc604249294d
Michael Fang <mikefang530@gmail.com>
2019-02-05 13:36:42 -0500

    trying to make candy pieces to go downwards

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const body = document.querySelector('body');
 // const instrBox = document.querySelector('.scoreInstr');
 const section = document.querySelector('section');
 const remBox = document.querySelector('.toRemove')
-// const dropperObject = document.querySelector('.candyDropper');
+const newCandyPiece = document.querySelector('theCandy')
 
 const removeInstrBox = () => {
   remBox.remove();
@@ -28,31 +28,41 @@ const startDropper = () => {
 
 ///Create the dropping candy piece
 const candyPiece = () => {
-  const theCandy = document.createElement('div');
+  theCandy = document.createElement('div');
   theCandy.classList.add('theCandy');
   board.appendChild(theCandy);
   return theCandy;
 };
 ///Set position for candyPiece
-
 const candyPosition = () => {
-  const candyPos = `${dropper.offsetLeft}px`;
 
-  let theCandy = candyPiece();
-  theCandy.style.left = candyPos;
-  console.log(candyPos);
+  const candyPos = `${dropper.offsetLeft}px`; //only a number
+  const theCandyMade = candyPiece(); // saving to the piece that was created
+  theCandyMade.style.left = candyPos;// setting the created piece to the px postion
+//  let newCandyPos = `${newCandyPiece.offsetTop + newCandyPiece.offsetHeight}px`;
 }
-//firing the candy down
-// const candyFire =(ev) => {
-//
-//   if (ev.keyCode === 32) {
-//     console.log("space bar pressed");
-//     let theCandy = candyPiece();
-//     theCandy.style.left = candyPostion();
-// };
-//
-// };
-  body.addEventListener('keydown', candyPosition);
+const candyFire =(ev) => {
+  if (ev.keyCode === 32) {
+      candyPosition();
+};
+};
+
+//movement for candy
+
+const dropCandy = () => {
+    //const currentCandyTop = `${theCandy.offsetTop}`;
+    //const currentCandyHeight = `${theCandy.offsetHeight}`;
+if (`${newCandyPiece.offsetTop + newCandyPiece.offsetHeight < board.offsetHeight}`) {
+  newCandyPiece.style.top = `${newCandyPiece.offsetTop}` + 5;
+  console.log(newCandyPiece.offsetTop);
+} else {
+  theCandy.remove();
+}
+setInterval(candyPiece, 20, dropCandy);
+}
+
+
+  body.addEventListener('keydown', candyFire);
 
 ////// Create the walls/////////////
 const mkWalls = () => {
