2d927257c8f88a382d876355e6a210993bfd9207
Michael Fang <mikefang530@gmail.com>
2019-02-05 20:49:40 -0500

    added walls to the board div instead of using dom

--- script.js
+++ script.js
@@ -7,14 +7,34 @@ const body = document.querySelector('body');
 // const instrBox = document.querySelector('.scoreInstr');
 const section = document.querySelector('section');
 const remBox = document.querySelector('.toRemove')
-const newCandyPiece = document.querySelector('theCandy')
+const newCandyPiece = document.querySelectorAll('theCandy')
+const wallLeft = document.querySelector('.wallLeft')
+const wallRight = document.querySelector('.wallRight')
 
 const removeInstrBox = () => {
   remBox.remove();
-  }
+
+// document.querySelectorAll("section").forEach(e => e.parentNode.removeChild(e))
+// for (i = 0; i < section.length; i++) {
+//   section.removeChild;
+}
 startButton.addEventListener('click', (removeInstrBox));
 
-//////////////Create the candy dropper//////////////
+//show walls display on
+const blockAppear = () => {
+  wallLeft.style.display = 'block';
+  wallRight.style.display = 'block';
+}
+
+//get wall Position
+const wallPos = (wall) => {
+wallLeftPos = wallLeft.offsetTop;
+//if (wallLeft.offsetTop + wallLeft.offsetHeight <
+
+}
+wallPos();
+console.log(wallPos);
+////////////Create the candy dropper//////////////
 const startDropper = () => {
 
   const dropperBox = document.createElement('div');
@@ -23,16 +43,17 @@ const startDropper = () => {
     dropper.classList.add('candyDropper');
     board.appendChild(dropperBox);
     dropperBox.appendChild(dropper);
-
 };
 
 ///Create the dropping candy piece
 const candyPiece = () => {
+
   theCandy = document.createElement('div');
   theCandy.classList.add('theCandy');
-  board.appendChild(theCandy);
+
   return theCandy;
 };
+
 //dropping candy//
 const moveCandy = (candy) => {
 
@@ -40,7 +61,7 @@ const moveCandy = (candy) => {
     candy.style.top = `${candy.offsetTop + 10}px`;
     console.log(candy.style.top);
   } else if (candy.offsetTop + candy.offsetHeight >= board.offsetHeight) {
-    console.log('works');
+    console.log('moving candy');
     candy.remove();
     clearInterval(timer);
 
@@ -48,56 +69,29 @@ const moveCandy = (candy) => {
 }
 ///Set position for candyPiece
 const candyPosition = () => {
+
   let candyPos = `${dropper.offsetLeft}px`; //only a number
   const theCandyMade = candyPiece(); // saving to the piece that was created
+  board.appendChild(theCandyMade);
   theCandyMade.style.left = candyPos;// setting the created piece to the px postion
   timer = setInterval(moveCandy, 20, theCandyMade);
 }
 const candyFire =(ev) => {
   if (ev.keyCode === 32) {
       candyPosition();
+}
 };
-};
-
-//dropping the candy
-
-// const dropCandy = () => {
-//     const currentCandy = newCandyPiece.offsetTop;
-//     console.log(currentCandy);
-//     const currentCandyHeight = `${newCandyPiece.offsetHeight}px`;
-// if (currentCandyTop + currentCandyHeight < `${board.offsetHeight}`) {
-//   newCandyPiece.style.top = `${newCandyPiece.offsetTop} + 5` ;
-// } else {
-//   theCandy.remove();
-// }
-// setInterval(candyPiece, 50, );
-// }
-
 const playGame = () => {
-body.addEventListener('keyup', candyFire);
+  body.addEventListener('keyup', candyFire);
 }
 playGame();
-////// Create the walls/////////////
-const mkWalls = () => {
-
-  const walls = document.createElement('div');
-  const wallLeft = document.createElement('div');
-  const wallRight = document.createElement('div');
-    walls.classList.add('walls');
-    wallLeft.classList.add('wallLeft');
-    wallRight.classList.add('wallRight');
-     board.appendChild(walls);
-     walls.appendChild(wallLeft);
-     walls.appendChild(wallRight);
-};
-
 //start the game //
 const beginGame = () => {
   startButton.addEventListener('click', startDropper);
-  startButton.addEventListener('click', mkWalls);
-//  startButton.addEventListener('click', candyPiece);
+  startButton.addEventListener('click', blockAppear);// must take display none back off
+  startButton.addEventListener('click', candyPiece);
+  startButton.addEventListener('click', (removeInstrBox));
 };
-
 beginGame();
 
 
